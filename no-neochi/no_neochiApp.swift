3ddf4a2e7d177b56cbfd3708759ad56dc219a216
saki0411 <chipdale411@gmail.com>
2024-08-23 01:09:35 +0900

    Initial Commit

---
+++ no-neochi/no_neochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  no_neochiApp.swift
+//  no-neochi
+//
+//  Created by saki on 2024/08/23.
+//
+
+import SwiftUI
+
+@main
+struct no_neochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
