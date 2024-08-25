c07b8d6609b82614763a38d2e90da2dab5c87af6
saki0411 <chipdale411@gmail.com>
2024-08-25 09:38:12 +0900

    formatter

--- no-neochi/formatter.swift
+++ no-neochi/formatter.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+class Formatter {
+    func formatDate(_ date: Date) -> String {
+        let calendar = Calendar.current
+        let formatter = DateFormatter()
+        
+        
+        formatter.dateFormat = "M/d"
+        let dateString = formatter.string(from: date)
+        
+        
+        let weekdayIndex = calendar.component(.weekday, from: date)
+        let weekdaySymbols = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."]
+        let weekday = weekdaySymbols[weekdayIndex - 1]
+        
+        
+        return "\(dateString) \(weekday)"
+    }
+    
+    
+    
+    func formatTime(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: date)
+    }
+}
