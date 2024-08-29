f9fa3e8eab36c5d6c34e3318a8211f727c02644e
saki0411 <chipdale411@gmail.com>
2024-08-29 18:54:01 +0900

    add-LoginTextView

---
+++ no-neochi/API/Login.swift
@@ -0,0 +1,8 @@
+//
+//  Login.swift
+//  no-neochi
+//
+//  Created by saki on 2024/08/29.
+//
+
+import Foundation
---
+++ no-neochi/View/login/LoginTextView.swift
@@ -0,0 +1,81 @@
+//
+//  LoginTextView.swift
+//  no-neochi
+//
+//  Created by saki on 2024/08/29.
+//
+
+import SwiftUI
+
+struct LoginTextView: View {
+    @State var email = ""
+    @State var password = ""
+    @State var conirmPassword = ""
+    @State var name = ""
+    var body: some View {
+        VStack{
+            Text("Email")
+                .font(.custom("ABeeZee Regular", size: 14))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            
+            TextField("", text: $email)
+                .border(Color(#colorLiteral(red: 0.886274516582489, green: 0.7764706015586853, blue: 1, alpha: 1)))
+                .font(.system(size: 30))
+            
+            
+                .padding(.bottom, 35)
+            Text("Password")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.custom("ABeeZee Regular", size: 14))
+            
+            
+            
+            TextField("", text: $password)
+                .border(Color(#colorLiteral(red: 0.886274516582489, green: 0.7764706015586853, blue: 1, alpha: 1)))
+                .font(.system(size: 30))
+            
+            
+                .padding(.bottom, 35)
+            //Password
+            Text("Password(confirm)")
+                .font(.custom("ABeeZee Regular", size: 14))
+                .frame(maxWidth: .infinity, alignment: .leading)
+    
+            TextField("", text: $conirmPassword)
+                .border(Color(#colorLiteral(red: 0.886274516582489, green: 0.7764706015586853, blue: 1, alpha: 1)))
+                .font(.system(size: 30))
+            
+            
+                .padding(.bottom, 35)
+            
+            //Name
+            Text("Name")
+                .font(.custom("ABeeZee Regular", size: 14))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            TextField("", text: $name)
+                .border(Color(#colorLiteral(red: 0.886274516582489, green: 0.7764706015586853, blue: 1, alpha: 1)))
+                .font(.system(size: 30))
+                .padding(.bottom, 35)
+            
+
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Sign Up").font(.custom("ABeeZee Regular", size: 14)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).multilineTextAlignment(.center)
+                    .padding(.horizontal,78)
+                    .padding(.vertical, 10)
+                    .background(Color(#colorLiteral(red: 0.886274516582489, green: 0.7764706015586853, blue: 1, alpha: 1)))
+                    .cornerRadius(10)
+                
+                
+            })
+        }
+        .padding(.horizontal,57)
+        
+        
+    }
+}
+
+#Preview {
+    LoginTextView()
+}
